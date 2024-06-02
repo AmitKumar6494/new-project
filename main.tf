3a5c51afd2990a15d0148874ba7abe9b8caf39fa
Amit Kumar <amitektn@gmail.com>
2024-06-02 23:45:21 +0530

    new rg

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "new-project-rg" {
+    name = "newproject-rg"
+    location = "central canada"
+  
+}
---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    source ="hashicorp/azurerm"
+    version = "106.6.1"
+  }
+}
+provider "azurerm" {
+    features {
+      
+    }
+  
+}
